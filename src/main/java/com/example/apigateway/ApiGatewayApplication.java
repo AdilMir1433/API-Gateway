55235859ba34a28dd8bc607e18274d47edd96008
AdilMir1433 <adil.mir@devsinc.com>
2023-08-01 15:34:52 +0500

    API Gateway

---
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient // This annotation is required for the API Gateway to be able to discover the services that it needs to route.
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
